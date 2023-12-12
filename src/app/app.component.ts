512994af4446fd2e5463525d91e33673c97ec2a7
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2023-12-12 11:04:22 +0100

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderNavComponent} from "./components/header-nav/header-nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
