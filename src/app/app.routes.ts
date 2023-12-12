7ea78a676145033ebf82f6a19b308d9f7156114d
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2023-12-12 12:23:33 +0100

    first route + cataleg component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CatalegComponent} from "./components/cataleg/cataleg.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cataleg', component:CatalegComponent}
+];
