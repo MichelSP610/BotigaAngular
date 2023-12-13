9521ca593484f2ed77bbc37ebcc3bbf240e737b6
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2023-12-13 14:34:22 +0100

    Cistella

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {CatalegComponent} from "./components/cataleg/cataleg.component";
 import {IniciComponent} from "./components/inici/inici.component";
 import {RegistreComponent} from "./components/registre/registre.component";
+import {CistellaComponent} from "./components/cistella/cistella.component";
 
 export const routes: Routes = [
   { path: '', component:IniciComponent},
   { path: 'cataleg', component:CatalegComponent},
-  { path: 'registre', component:RegistreComponent}
+  { path: 'registre', component:RegistreComponent},
+  {path: 'cistella', component:CistellaComponent}
 ];
