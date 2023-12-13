769d18c8774fcc803f05bd8002a136890f51fc80
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2023-12-13 13:47:59 +0100

    default routes + inici creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {CatalegComponent} from "./components/cataleg/cataleg.component";
+import {IniciComponent} from "./components/inici/inici.component";
 
 export const routes: Routes = [
-  { path: 'cataleg', component:CatalegComponent}
+  { path: 'cataleg', component:CatalegComponent},
+  { path: '', component:IniciComponent}
 ];
---
+++ src/app/components/inici/inici.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inici',
+  standalone: true,
+  imports: [],
+  templateUrl: './inici.component.html',
+  styleUrl: './inici.component.css'
+})
+export class IniciComponent {
+
+}
