8a449940f99748fc9c6c51dec36d30a1beff76f9
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2023-12-13 14:22:21 +0100

    nav fix + registre creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {CatalegComponent} from "./components/cataleg/cataleg.component";
 import {IniciComponent} from "./components/inici/inici.component";
+import {RegistreComponent} from "./components/registre/registre.component";
 
 export const routes: Routes = [
+  { path: '', component:IniciComponent},
   { path: 'cataleg', component:CatalegComponent},
-  { path: '', component:IniciComponent}
+  { path: 'registre', component:RegistreComponent}
 ];
