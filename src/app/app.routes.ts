24d4da245225b613ddf62cbad4dcd69a07a3c5d8
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2023-12-14 08:21:02 +0100

    Contacte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {CatalegComponent} from "./components/cataleg/cataleg.component";
 import {IniciComponent} from "./components/inici/inici.component";
 import {RegistreComponent} from "./components/registre/registre.component";
 import {CistellaComponent} from "./components/cistella/cistella.component";
+import {ContacteComponent} from "./components/contacte/contacte.component";
 
 export const routes: Routes = [
   { path: '', component:IniciComponent},
   { path: 'cataleg', component:CatalegComponent},
   { path: 'registre', component:RegistreComponent},
-  {path: 'cistella', component:CistellaComponent}
+  {path: 'cistella', component:CistellaComponent},
+  {path: 'contacte', component:ContacteComponent}
 ];
