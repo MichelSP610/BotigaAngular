4d7318295441fd17ed4a53ff2f3b7d5789f6fe14
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-01-06 16:12:55 +0100

    user info page deleted + buttons fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,6 @@ import {ContacteComponent} from "./components/contacte/contacte.component";
 import {CondicionsComponent} from "./components/condicions/condicions.component";
 import {PrivacitatComponent} from "./components/privacitat/privacitat.component";
 import {IniciSessioComponent} from "./components/inici-sessio/inici-sessio.component";
-import {InformacioUsuariComponent} from "./components/informacio-usuari/informacio-usuari.component";
 
 export const routes: Routes = [
   { path: '', component:IniciComponent},
@@ -17,6 +16,5 @@ export const routes: Routes = [
   {path: 'contacte', component:ContacteComponent},
   {path: 'condicions', component:CondicionsComponent},
   {path: 'privacitat', component:PrivacitatComponent},
-  {path: 'login', component:IniciSessioComponent},
-  {path: 'user-info', component:InformacioUsuariComponent}
+  {path: 'login', component:IniciSessioComponent}
 ];
