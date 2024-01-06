26085fa5c1f72c32af2c33059715150d89cdafca
MichelSaez610 <michelsaez@institutvidreres.cat>
2024-01-06 15:36:16 +0100

    Afegir productes del cataleg a la cistella, eliminar productes de la cistella i al presionar comprar s'eliminen tots els productes de la cistella.

--- src/app/components/cataleg/cataleg.component.ts
+++ src/app/components/cataleg/cataleg.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import {FooterComponent} from "../footer/footer.component";
-
-
+import {Component, inject} from '@angular/core';
+import {FooterComponent} from "../footer/footer.component"
+import {CistellaService} from "../../serveis/cistella.service";
+import {CurrencyPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-cataleg',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, NgForOf, CurrencyPipe],
   templateUrl: './cataleg.component.html',
   styleUrl: './cataleg.component.css'
 })
 export class CatalegComponent {
+  cartService = inject(CistellaService)
+
+  products: any[] = [
+    {name: 'Galetes de llet condensada', id: 1, quantity: 1, image: 'assets/condensed-milk-cookies.jpg', price: 9.99, description: '1kg de galetes de llet condensada de la marca Fornais'},
+    {name: 'Galetes de chocolata', id: 2, quantity: 1, image: 'assets/chocolate-chip-cookies.jpg', price: 12.99, description: '1kg de galetes de chocolata de la marca Fornais'},
+    {name: 'Galetes de integrals', id: 3, quantity: 1, image: 'assets/integral-cookies.jpg', price: 14.99, description: '1kg de galetes itegrals de la marca Fornais'},
+    {name: 'Galetes de llet condensada', id: 4, quantity: 1, image: 'assets/condensed-milk-cookies.jpg', price: 9.99, description: '1kg de galetes de llet condensada de la marca Fornais'},
+    {name: 'Galetes de chocolata', id: 5, quantity: 1, image: 'assets/chocolate-chip-cookies.jpg', price: 12.99, description: '1kg de galetes de chocolata de la marca Fornais'},
+    {name: 'Galetes de integrals', id: 6,  quantity: 1, image: 'assets/integral-cookies.jpg', price: 14.99, description: '1kg de galetes itegrals de la marca Fornais'}
+  ];
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
+
+  trackByProductId(product: any): number {
+    return product.id;
+  }
+
 
 }
---
+++ src/app/serveis/cistella.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CistellaService {
+  private items: any[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+  addToCart(product: any) {
+    this.items.push(product);
+
+    localStorage.setItem('cartItems', JSON.stringify(this.items))
+  }
+
+  deleteFromCart(item: any) {
+    this.items = this.items.filter((i) => i.id !== item.id)
+
+    localStorage.setItem('cartItems', JSON.stringify(this.items))
+  }
+
+  incrementQuantity(id: number) {
+    let item = this.items.find((i) => i.id === id)
+    if (item) {
+      item.quantity++
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(this.items))
+  }
+
+  lowerQuantity(id: number) {
+    let item = this.items.find((i) => i.id === id)
+    if (item) {
+      item.quantity--
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(this.items))
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.price * item.quantity
+    }, 0)
+  }
+
+  deleteCart() {
+    this.items = []
+
+    localStorage.setItem('cartItems', JSON.stringify(this.items))
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
