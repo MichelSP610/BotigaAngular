3a3f25c59c33531137d3e4ea84c83526f44c52bb
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-02-15 10:47:53 +0100

    proves eliminades

--- src/app/components/header-nav/header-nav.component.ts
+++ src/app/components/header-nav/header-nav.component.ts
@@ -15,18 +15,6 @@ export class HeaderNavComponent {
 
   username: any;
   constructor(private sessioService: SessioService, private router: Router, private http: HttpClient) {
-
-    let req = new HttpParams().set('id', 1);
-
-    this.http.get<any>('http://localhost:3080/getClientById', {params: req}).subscribe((client) => {
-    console.log(client)
-    })
-
-    let req2 = {username: 'admin', password: 'patata'}
-
-    this.http.get<any>('http://localhost:3080/getClientByName', {params: req2}).subscribe((client) => {
-      console.log(client.id)
-    })
   }
 
   ngOnInit() {
