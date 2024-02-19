8d25b3050b54e312d9c507fddb85d4204f4435fd
MichelSaez610 <michelsaez@institutvidreres.cat>
2024-02-19 10:30:45 +0100

    Inici de la pagina del perfil.

--- src/app/components/perfil-usuari/perfil-usuari.component.ts
+++ src/app/components/perfil-usuari/perfil-usuari.component.ts
@@ -1,19 +1,69 @@
 import { Component } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { SessioService } from "../../serveis/sessio.service";
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule, HttpParams } from "@angular/common/http";
 
 @Component({
   selector: 'app-perfil-usuari',
   standalone: true,
     imports: [
-        FormsModule,
-        ReactiveFormsModule,
+        RouterOutlet,
+        CommonModule,
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        HttpClientModule
     ],
   templateUrl: './perfil-usuari.component.html',
   styleUrl: './perfil-usuari.component.css'
 })
 export class PerfilUsuariComponent {
+
+  username: any;
+  password: any;
+  email: any;
+  name: any;
+  lastname: any;
+
+  constructor(private sessioService: SessioService, private http: HttpClient) {
+    let user = sessionStorage.getItem('username');
+    let password = sessionStorage.getItem('password');
+
+    if (user && password) {
+      let req = {username: user, password: password};
+
+      this.http.get<any>('http://localhost:3080/getClientByName', {params: req}).subscribe((client) => {
+        console.log('Respuesta del servidor:', client);
+
+        sessionStorage.setItem('username', client.username);
+        sessionStorage.setItem('password', client.password);
+        sessionStorage.setItem('email', client.email);
+        sessionStorage.setItem('name', client.name);
+        sessionStorage.setItem('lastname', client.lastname);
+
+        // Asigna los valores a las propiedades del componente
+        this.username = client.username;
+        this.password = client.password;
+        this.email = client.email;
+        this.name = client.name;
+        this.lastname = client.lastname;
+      });
+    }
+  }
+
+  ngOnInit() {
+    console.log(this.username)
+  }
+
+
+  readUserConnection():string {
+    let userSession = sessionStorage.getItem('userConnected')
+    if (!userSession || userSession === 'false') {
+      return 'false'
+    } else {
+      this.username = this.sessioService.getUserName(sessionStorage.getItem('userPos'))
+      return 'true';
+    }
+  }
 
 }
