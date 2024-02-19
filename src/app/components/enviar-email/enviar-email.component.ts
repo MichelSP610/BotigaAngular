8450589dc444c8aca0f2dcd6453ed7c8b89663e5
MichelSaez610 <michelsaez@institutvidreres.cat>
2024-02-19 12:04:39 +0100

    css de correu de recuperacio finalitzat.

--- src/app/components/enviar-email/enviar-email.component.ts
+++ src/app/components/enviar-email/enviar-email.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import {ReactiveFormsModule, FormBuilder, FormsModule} from "@angular/forms";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 
@@ -16,15 +16,22 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 export class EnviarEmailComponent {
 
   input
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+  popup: any;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private el: ElementRef) {
     this.input = this.formBuilder.group({
       email: ''
     });
+  }
+
+  showPopup() {
+    let popup = this.el.nativeElement.querySelector("#myPopup");
+    popup.classList.toggle("show");
   }
 
   onSubmit() {
     let data = {email: this.input.value.email}
     //this.http.post('http://172.16.9.1:3080/sendEmail', data).subscribe()
     this.http.post('http://localhost:3080/sendEmail', data).subscribe()
+    this.showPopup()
   }
 }
