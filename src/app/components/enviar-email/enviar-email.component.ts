433f262c51bbb44a0386b32a7979b339fba260d8
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-02-19 18:58:57 +0100

    Modificat el registre per evitar correus repetits i recuperacio de contrasenya funcional

--- src/app/components/enviar-email/enviar-email.component.ts
+++ src/app/components/enviar-email/enviar-email.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef } from '@angular/core';
 import {ReactiveFormsModule, FormBuilder, FormsModule} from "@angular/forms";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {SessioService} from "../../serveis/sessio.service";
 
 @Component({
   selector: 'app-enviar-email',
@@ -17,21 +18,30 @@ export class EnviarEmailComponent {
 
   input
   popup: any;
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private el: ElementRef) {
+  emailError: any;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private el: ElementRef, private sessioService: SessioService) {
     this.input = this.formBuilder.group({
       email: ''
     });
   }
-
   showPopup() {
     let popup = this.el.nativeElement.querySelector("#myPopup");
     popup.classList.toggle("show");
   }
 
-  onSubmit() {
-    let data = {email: this.input.value.email}
-    //this.http.post('http://172.16.9.1:3080/sendEmail', data).subscribe()
-    this.http.post('http://localhost:3080/sendEmail', data).subscribe()
-    this.showPopup()
+  async onSubmit() {
+    if (await this.sessioService.checkUserByEmail(this.input.value.email)) {
+      let data = {email: this.input.value.email}
+      //this.http.post('http://172.16.9.1:3080/sendEmail', data).subscribe()
+      this.http.post('http://localhost:3080/sendEmail', data).subscribe()
+      this.input.reset();
+      this.showPopup()
+    }
+    else {
+      this.input.reset();
+      this.emailError = document.getElementById("email-error");
+      this.emailError.innerHTML = "Usuari o contrasenya incorrectes";
+    }
   }
+
 }
