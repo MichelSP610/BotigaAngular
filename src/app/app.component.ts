cc159058bb497b88e58f65804457624a0119416b
MichelSaez610 <michelsaez@institutvidreres.cat>
2024-03-14 08:57:13 +0100

    checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
+
 import {HeaderNavComponent} from "./components/header-nav/header-nav.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {CatalegComponent} from "./components/cataleg/cataleg.component";
 import { FiltreGaletesPipe} from "./components/pipes/filtre-galetes.pipe";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
 
 
 @Component({
@@ -25,7 +24,6 @@ export class AppComponent {
   imports: [
     CommonModule,
     BrowserModule,
-    NgbModule,
     FormsModule,
   ],
   bootstrap: [AppComponent],
