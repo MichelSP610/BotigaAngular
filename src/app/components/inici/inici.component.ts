9e4291f5ccc495d03eb8626584f310cbf47e3f2a
MichelSaez610 <michelsaez@institutvidreres.cat>
2024-03-14 11:09:50 +0100

    Bootstrap Inici Taula

--- src/app/components/inici/inici.component.ts
+++ src/app/components/inici/inici.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
+import {NgbModal, NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-inici',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, NgbModule],
   templateUrl: './inici.component.html',
   styleUrl: './inici.component.css'
 })
 export class IniciComponent {
 
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
