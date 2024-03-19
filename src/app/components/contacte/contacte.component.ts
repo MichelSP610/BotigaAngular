26e327172a119ff2b581f1da844f5f3f0d9150b3
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2024-03-19 20:44:33 +0100

    Captcha formulari de registre i formulari de contacte

--- src/app/components/contacte/contacte.component.ts
+++ src/app/components/contacte/contacte.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
+import {RecaptchaModule} from "ng-recaptcha";
 
 @Component({
   selector: 'app-contacte',
   standalone: true,
-  imports: [],
+  imports: [RecaptchaModule],
   templateUrl: './contacte.component.html',
   styleUrl: './contacte.component.css'
 })
 export class ContacteComponent {
+  recaptcha: string | null | undefined;
+  onResolved(captchaResponse: string | null){
+    this.recaptcha=captchaResponse;
+    console.log(this.recaptcha);
+  }
 
 }
--- src/app/components/registre/registre.component.ts
+++ src/app/components/registre/registre.component.ts
@@ -3,16 +3,21 @@ import {RouterLink, RouterLinkActive, RouterOutlet, Router} from "@angular/route
 import {SessioService} from "../../serveis/sessio.service";
 import {Form, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FormBuilder} from '@angular/forms';
+import {RecaptchaModule} from "ng-recaptcha";
 
 @Component({
   selector: 'app-registre',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, FormsModule, ReactiveFormsModule, RecaptchaModule],
   templateUrl: './registre.component.html',
   styleUrl: './registre.component.css'
 })
 export class RegistreComponent {
-
+  recaptcha: string | null | undefined;
+  onResolved(captchaResponse: string | null){
+    this.recaptcha=captchaResponse;
+    console.log(this.recaptcha);
+  }
   errorP: any;
   input;
   constructor(private formBuilder: FormBuilder, private sessioService: SessioService, private router: Router) {
