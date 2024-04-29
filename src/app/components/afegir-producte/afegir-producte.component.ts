5276c98e6821196e425aa591f3e600a2b3dd32b6
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-04-29 16:30:44 +0200

    first chart

---
+++ src/app/components/afegir-producte/afegir-producte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-afegir-producte',
+  standalone: true,
+  imports: [],
+  templateUrl: './afegir-producte.component.html',
+  styleUrl: './afegir-producte.component.css'
+})
+export class AfegirProducteComponent {
+
+}
