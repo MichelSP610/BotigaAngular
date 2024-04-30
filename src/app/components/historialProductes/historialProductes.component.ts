ae22affe494b8b94eb2bc7a6219060782ef39e66
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-04-30 15:35:18 +0200

    fixed ngIf not working

--- src/app/components/historialProductes/historialProductes.component.ts
+++ src/app/components/historialProductes/historialProductes.component.ts
@@ -1,4 +1,4 @@
-
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 // @ts-ignore
@@ -7,7 +7,7 @@ import { Productesvenuts } from '../models/detall.js';
 @Component({
   selector: 'app-historial-productes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './historialProductes.component.html',
   styleUrl: './historialProductes.component.css'
 })
@@ -24,7 +24,10 @@ export class HistorialProductesComponent {
   obtenerProductos(): void {
     this.http.get<Productesvenuts[]>('http://localhost:3080/historial').subscribe(
       (data: Productesvenuts[]) => {
+        console.log("Getting detalls successful")
+        console.log("data: " + data)
         this.historialProductos = data;
+        console.log(this.historialProductos)
       },
       (err) => {
         console.error('Error al obtener el historial de productos:', err);
