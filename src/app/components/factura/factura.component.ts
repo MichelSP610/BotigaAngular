73c274c85ec31320b18a61078d7ee922975d4cc2
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-04-30 17:13:00 +0200

    factura attempt

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-factura',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
 export class FacturaComponent {
+  factures = []
+
+  constructor(private http: HttpClient){
+    this.getFactures().then((data) => {
+      //@ts-ignore
+      this.factures = data;
+    })
+  }
+
+  async getFactures() {
+    this.http.get('http://localhost:3080/getFactures').subscribe((data) => {
+      return data;
+    })
+  }
 
 }
