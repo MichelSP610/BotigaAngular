118c16b95449f39043afe84432412e1ac28f2485
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-04-30 19:45:46 +0200

    factures functionality working only css left

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -10,19 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './factura.component.css'
 })
 export class FacturaComponent {
-  factures = []
+  factures: any
 
   constructor(private http: HttpClient){
-    this.getFactures().then((data) => {
-      //@ts-ignore
-      this.factures = data;
-    })
+    this.getFactures()
   }
 
   async getFactures() {
     this.http.get('http://localhost:3080/getFactures').subscribe((data) => {
-      return data;
+      //@ts-ignore
+      this.factures = data;
+      console.log(this.factures)
     })
+  }
+
+  getFacturaTotal(factura: any) {
+    let preuFinal = 0
+    factura.data.forEach((producte: any) => {
+      if (producte.detall_oferta == null || producte.detall_oferta == 0) {
+        preuFinal += producte.detall_preu * producte.detall_quantitat;
+      } else {
+        preuFinal += (producte.detall_preu * (100 * producte.detall_oferta)) * producte.detall_quantitat;
+      }
+    })
+    return preuFinal;
   }
 
 }
