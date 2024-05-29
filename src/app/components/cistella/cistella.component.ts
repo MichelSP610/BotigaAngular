700ab32f921c541d34f72d5ca659fe107e61f6cd
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-05-29 08:21:10 +0200

    1st buying attempt (cost not calculated propertly)

--- src/app/components/cistella/cistella.component.ts
+++ src/app/components/cistella/cistella.component.ts
@@ -82,4 +82,60 @@ export class CistellaComponent {
   trackByProductId(product: any): number {
     return product.id;
   }
+
+  guardarCompra() {
+    this.comprar().then(r => {
+      this.cartService.guardarCompra()
+    });
+  }
+
+  async comprar() {
+    //@ts-ignore
+    if (typeof window.ethereum !== 'undefined') {
+      const total = this.getConvertedTotal();
+      const params = {
+        from: await this.getCurrentAccount(),
+        to: '0x349A39B4660Fb4b4a41a2592390F9E3BCddb72c3',
+        gas: '0x76c0',
+        gasPrice: '0x9184e72a000',
+        value: this.selectedCrypto === 'eur' ? this.toWei(total, 18) : this.toWei(total, this.getDecimalPlaces(this.selectedCrypto))
+      };
+
+      try {
+        //@ts-ignore
+        const result = await window.ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [params],
+        });
+        console.log('Transaction successful with hash:', result);
+      } catch (error) {
+        console.error('Error during transaction:', error);
+      }
+    } else {
+      console.error('MetaMask is not installed!');
+    }
+  }
+
+  private async getCurrentAccount(): Promise<string> {
+    //@ts-ignore
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    return accounts[0];
+  }
+
+  private toWei(amount: number, decimals: number): string {
+    return (amount * Math.pow(10, decimals)).toString();
+  }
+
+  private getDecimalPlaces(crypto: string): number {
+    switch (crypto) {
+      case 'bitcoin':
+        return 8;
+      case 'ethereum':
+        return 18;
+      case 'binancecoin':
+        return 18;
+      default:
+        return 18;
+    }
+  }
 }
